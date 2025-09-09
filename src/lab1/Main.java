ec722c03cb04fb374ca07dd5eda1052d9e9a4bae
Gabriel <75056711+gkgeo9@users.noreply.github.com>
2025-09-09 11:23:55 -0400

    Creating Main.java and modified FizzBuzz to include a method

---
+++ src/lab1/Main.java
@@ -0,0 +1,9 @@
+package lab1;
+
+public class Main {
+    public static void main(String[] args) {
+        FizzBuzz fb = new FizzBuzz();
+        fb. doFizzBuzz(5);
+        FizzBuzz.main();
+    }
+}
